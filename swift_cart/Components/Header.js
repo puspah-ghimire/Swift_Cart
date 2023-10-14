e363084dc4c7e0557af0d76cc53249e8710e83cc
Aayush <aayushman950@gmail.com>
2023-10-15 00:14:23 +0545

    added searchbox in navbar

--- swift_cart/Components/Header.js
+++ swift_cart/Components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+import Searchbar from './Searchbar'
 
 const Header = () => {
   return (
     <div className=' bg-swiftcart-primary flex items-center justify-between p-8 text-white'>
         <Link href="/">Swift Cart</Link>
+
+        <Searchbar></Searchbar>
         
         <div className='flex gap-6'>
             <Link href="/MyCart">Cart</Link>
