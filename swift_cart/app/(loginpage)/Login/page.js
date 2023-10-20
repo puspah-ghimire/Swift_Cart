618c2d9c9ed6c5afbe4767389a4a99b2b36c8628
Aayush <aayushman950@gmail.com>
2023-10-20 21:16:59 +0545

    added working sign up page

--- swift_cart/app/(loginpage)/Login/page.js
+++ swift_cart/app/(loginpage)/Login/page.js
@@ -1,34 +1,70 @@
-import React from 'react'
+"use client"
 
-const loginpage = () => {
+import React, { useState } from 'react'
+import { auth, googleAuth } from '@/app/firebase/firebase'
+import { signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
+import Link from 'next/link'
+
+const loginForm = () => {
+  const [username, setUsername] = useState(null)
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
+
+  console.log(auth?.currentUser?.email)
+
+  const signInHandler = async () => {
+    if (!email || !password) return
+    try { const user = await signInWithEmailAndPassword(auth, email, password) }
+    catch(error) { console.error(error) }
+  }
+
+  const signInGoogleHandler = async () => {
+    try { const user = await signInWithPopup(auth, googleAuth) }
+    catch(error) { console.error(error) }
+  }
+  
+  const logout = async () => {
+    try { await signOut(auth) }
+    catch(error) { console.error(error) }
+  }
+
   return (
-    <>
-      <body className=' bg-swiftcart-light'>   
-        <div className=' bg-white border-swiftcart-dark border-2 flex flex-col p-8 my-12 mx-24 items-center rounded-3xl'>
-            <div className='flex flex-col items-center'>
-              <h1 className=' font-extrabold text-3xl p-4'>Login to Swift Cart</h1>
-              <input type="text" 
-              placeholder='email address'
-              className=' mb-2 text-xl px-12 py-2 mt-4 rounded-full bg-swiftcart-lighter'/>
-              <input type="text" 
-              placeholder='password'
-              className=' mb-2 text-xl px-12 py-2 rounded-full bg-swiftcart-lighter'/>
-              <button className='bg-swiftcart-primary text-white px-10 py-2 rounded-full font-semibold m-4 mb-2 hover:bg-swiftcart-dark'>LOGIN</button>
-              <h3 className=' text-xs text-gray-500'>Forgot Password?</h3>
-            </div>
-
-            <div>
-              <button className='bg-swiftcart-primary text-white px-10 py-2 rounded-lg font-semibold m-4 mb-2 hover:bg-swiftcart-dark'>Sign in with Google</button>
-            </div>
-
-            <div className='flex items-center gap-4 my-4'>
-              <h2 className=' font-medium'>New to Swift Cart?</h2>
-              <button className='bg-swiftcart-primary text-white px-10 py-2 rounded-full font-semibold hover:bg-swiftcart-dark'>Sign Up</button>
-            </div>
-        </div>
-        </body> 
+    <>  
+    <div className=' bg-white border-swiftcart-dark border-2 flex flex-col p-8 my-12 mx-24 items-center rounded-3xl'>
+      <form onSubmit={e => e.preventDefault()}>
+          <div className='flex flex-col items-center'>
+            <h1 className=' font-extrabold text-3xl p-4'>Login to Swift Cart</h1>
+            <label>
+            <input type="email" required
+            placeholder='email address'
+            onChange = {(e) => {setEmail(e.target.value)}}
+            className=' mb-2 text-xl px-12 py-2 mt-4 rounded-full bg-swiftcart-lighter'/>
+            </label>
+            <input type="password" required
+            placeholder='password'
+            onChange = {(e) => {setPassword(e.target.value)}}
+            className=' mb-2 text-xl px-12 py-2 rounded-full bg-swiftcart-lighter'/>
+            <button 
+            onClick={signInHandler}
+            className='bg-swiftcart-primary text-white px-10 py-2 rounded-full font-semibold m-4 mb-2 hover:bg-swiftcart-dark'>LOGIN</button>
+            <h3 className=' text-xs text-gray-500'>Forgot Password?</h3>
+          </div>
+    </form>
+          <div>
+            <button 
+            onClick={signInGoogleHandler}
+            className='bg-swiftcart-primary text-white px-10 py-2 rounded-lg font-semibold m-4 mb-2 hover:bg-swiftcart-dark'>Sign in with Google</button>
+          </div>
+          <div className='flex items-center gap-4 my-4'>
+            <h2 className=' font-medium'>New to Swift Cart?</h2>
+            <button
+            className='bg-swiftcart-primary text-white px-10 py-2 rounded-full font-semibold hover:bg-swiftcart-dark'>
+              Sign Up
+            </button>
+          </div>
+      </div>
     </>
   )
 }
 
-export default loginpage
+export default loginForm
---
+++ swift_cart/app/(loginpage)/SignUp/page.js
@@ -0,0 +1,70 @@
+"use client"
+
+import React from 'react'
+import { useState } from 'react'
+import { auth, googleAuth } from '@/app/firebase/firebase'
+import { createUserWithEmailAndPassword, updateProfile, signInWithPopup, signOut } from 'firebase/auth'
+
+const SignUpPage = () => {
+
+  const [username, setUsername] = useState(null)
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
+
+  const signUphandler = async () => {
+    if (!username || !email || !password) return
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, password)
+      await updateProfile(auth.currentUser, {
+        displayName: username
+      })
+      console.log(user) }
+    catch(error) { console.error(error) }
+  }
+
+  const signInGoogleHandler = async () => {
+    try { const user = await signInWithPopup(auth, googleAuth) }
+    catch(error) { console.error(error) }
+  }
+  
+
+  return (
+    <>
+      <button
+      onClick={signInGoogleHandler}
+      className=' bg-swiftcart-dark'>Sign In with Google</button>
+
+      <form onSubmit={e => e.preventDefault()}>
+        <div>
+            <label>username
+            <input type="text" required
+                className=' bg-red-400'
+                onChange={e => setUsername(e.target.value)} />
+            </label>
+        </div>
+
+        <div>
+            <label>email
+            <input type="email" required
+                className=' bg-red-400' 
+                onChange={e => setEmail(e.target.value)}/>
+            </label>
+        </div>
+
+        <div>
+            <label>password
+            <input type="password" required
+                className=' bg-red-400'
+                onChange={e => setPassword(e.target.value)} />
+            </label>
+        </div>
+
+        <button 
+        onClick={signUphandler}
+        className=' bg-swiftcart-primary'>Sign Up</button>
+      </form>
+    </>
+  )
+}
+
+export default SignUpPage
