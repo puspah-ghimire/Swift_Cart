41f4444083ed1d10ef957b370d68f8444bba4475
Puspa <edith.puspa@gmail.com>
2023-12-12 16:54:07 +0545

    Revert "ALL_PRODUCT_SUCCESS is corrected and the proxy is setup in get request"
    This reverts commit a167adc95b200dd666b69db69640badf1898e259.

--- frontend/src/actions/productAction.jsx
+++ frontend/src/actions/productAction.jsx
@@ -4,7 +4,7 @@ import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_FAIL, CLEAR_ERROR
 export const getProduct = () => async(dispatch)=>{
   try {
     dispatch({type: ALL_PRODUCT_REQUEST})
-    const {data} = await axios.get("http://localhost:4000/api/v1/products")
+    const {data} = await axios.get("/api/v1/products")
 
     dispatch({
       type:ALL_PRODUCT_SUCCESS,
