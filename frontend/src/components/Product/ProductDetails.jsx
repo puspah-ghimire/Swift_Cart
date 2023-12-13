c005e0e1cdea4976de13ba7a83813da2691fb085
Puspa <edith.puspa@gmail.com>
2023-12-13 17:25:42 +0545

    Added single product functionality in productDetails and updated productSlice

--- frontend/src/components/Product/ProductDetails.jsx
+++ frontend/src/components/Product/ProductDetails.jsx
@@ -1,61 +1,48 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Carousel from 'react-material-ui-carousel';
 import { useDispatch, useSelector } from 'react-redux';
-import { getProducts } from '../../redux/productSlice';
-import ReactStars from "react-rating-stars-component"
+import { fetchProductDetails } from '../../redux/productSlice';
+import ReactStars from 'react-rating-stars-component';
 import { options } from '../Home/Product';
+import { useParams } from 'react-router-dom';
 
 const ProductDetails = () => {
   const dispatch = useDispatch();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const products = useSelector((state) => state.products.products);
+  const { id } = useParams();
+  const cleanedId = id.replace(':', '');
+  const productDetails = useSelector((state) => state.products.productDetails);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:4000/api/v1/products');
-        dispatch(getProducts(response.data));
-        setLoading(false);
-      } catch (err) {
-        console.error(err);
-        setError('Error fetching data');
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, [dispatch]);
+    dispatch(fetchProductDetails(cleanedId));
+  }, [dispatch, cleanedId]);
 
-  if (loading) {
+  if (!productDetails) {
     return <p>Loading...</p>; // You can replace this with a loading spinner or component
   }
-
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
-
-  const selectedProduct = products[0]; // Display details for the first product
 
   return (
     <>
       <div className='flex flex-col md:flex-row md:p-12 md:py-20 p-6'>
         <div className='md:w-1/2 mb-4'>
-          <Carousel className=' border-2 h-[calc(50vmin)]'>
-            {selectedProduct && (
-              <img src={selectedProduct.images} alt={`Slide`} key={selectedProduct.id} />
+          <Carousel className='border-2 h-[calc(50vmin)]'>
+            {productDetails && (
+              <img src={productDetails.images} alt={`Slide`} key={productDetails.id} />
             )}
           </Carousel>
         </div>
         <div className='flex flex-col md:mx-14'>
-          <div className=' mb-4 flex flex-col'>
-            <p className='font-bold text-xl'>{selectedProduct.name}</p>
-            <p className=' font-bold text-2xl'>${selectedProduct.price}</p>
-            <p>{selectedProduct.description}</p>
-            <p><ReactStars {...options} /></p>
-            <p>Retailer: {selectedProduct.retailer}</p>
+          <div className='mb-4 flex flex-col'>
+            <p className='font-bold text-xl'>{productDetails.name}</p>
+            <p className='font-bold text-2xl'>${productDetails.price}</p>
+            <p>{productDetails.description}</p>
+            <div>
+              <ReactStars {...options} />
+            </div>
+            <p>Retailer: {productDetails.retailer}</p>
           </div>
-          <button className=' bg-blue-800 hover:bg-blue-900 text-white w-fit px-4 font-bold py-2 rounded-lg'>Add to Cart</button>
+          <button className='bg-blue-800 hover:bg-blue-900 text-white w-fit px-4 font-bold py-2 rounded-lg'>
+            Add to Cart
+          </button>
         </div>
       </div>
     </>
--- frontend/src/redux/productSlice.jsx
+++ frontend/src/redux/productSlice.jsx
@@ -1,15 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const productSlice = createSlice({
-    name: "products",
-    initialState:{
-        products:[]
+  name: 'products',
+  initialState: {
+    products: [],
+    productDetails: null,
+  },
+  reducers: {
+    getProducts: (state, action) => {
+      state.products = action.payload;
     },
-    reducers: {
-        getProducts: (state, action) => {
-          state.products = action.payload;
-        },
-      },
-    });
-export const {getProducts} = productSlice.actions;
+    getSingleProduct: (state, action) => {
+      state.productDetails = action.payload;
+    },
+  },
+});
+
+export const { getProducts, getSingleProduct } = productSlice.actions;
+
+export const fetchProductDetails = (productId) => async (dispatch) => {
+  try {
+    const response = await axios.get(`http://localhost:4000/api/v1/product/${productId}`);
+    dispatch(getSingleProduct(response.data));
+  } catch (error) {
+    console.error('Error fetching product details:', error);
+  }
+};
+
 export default productSlice.reducer;
