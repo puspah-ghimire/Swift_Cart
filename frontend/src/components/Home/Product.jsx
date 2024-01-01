f4ffa7f26cff9d8e1aa1d738df4f74cb78d6102a
Aayush <aayushman950@gmail.com>
2024-01-01 17:55:33 +0545

    only logged in users can access cart

--- frontend/src/components/Home/Product.jsx
+++ frontend/src/components/Home/Product.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import ReactStars from "react-rating-stars-component"
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchUserData } from '../../redux/userSlice';
 
 export const options = {
   edit: false,
@@ -12,23 +15,46 @@ export const options = {
 }
 
 const Product = ( {product} ) => {
-  const navigate = useNavigate()
-  return (
-    <>
-    <Link to={`/product/:${product.id}`} className=' bg-gray-300 flex flex-col items-start justify-center p-2 md:w-[calc(14vmax)] border-2 hover:border-blue-500 w-[calc(40vmin)] min-h-[calc(20vmin)]'>
-      <img src={product.images} alt={product.name} className=' h-1/2 self-center'/>
-      <p><span className=' font-bold text-lg'>{product.name}</span></p>
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.userDetails);
 
-      <div className='md:flex items-center w-full justify-between'>
-        <ReactStars {...options}/> <span className=' font-extralight text-sm'>(256 reviews)</span>
-      </div>
+  useEffect(() => {
+    dispatch(fetchUserData());
+  }, [dispatch]);
 
-      <span className=' font-bold text-lg mb-2'>${product.price}</span>
-
-      <Link to={'/cart'} className=' bg-blue-500 text-white hover:bg-blue-600 py-4 w-full rounded-lg text-center font-bold'>Add To Cart</Link>
-    </Link>
-    </>
-  )
+  if (!user){
+    return (
+      <>
+      <Link to={`/product/:${product.id}`} className=' bg-gray-300 flex flex-col items-start justify-center p-2 md:w-[calc(14vmax)] border-2 hover:border-blue-500 w-[calc(40vmin)] min-h-[calc(20vmin)]'>
+        <img src={product.images} alt={product.name} className=' h-1/2 self-center'/>
+        <p><span className=' font-bold text-lg'>{product.name}</span></p>
+  
+        <div className='md:flex items-center w-full justify-between'>
+          <ReactStars {...options}/> <span className=' font-extralight text-sm'>(256 reviews)</span>
+        </div>
+  
+        <span className=' font-bold text-lg mb-2'>${product.price}</span>
+      </Link>
+      </>
+    )
+  } else {
+    return (
+      <>
+      <Link to={`/product/:${product.id}`} className=' bg-gray-300 flex flex-col items-start justify-center p-2 md:w-[calc(14vmax)] border-2 hover:border-blue-500 w-[calc(40vmin)] min-h-[calc(20vmin)]'>
+        <img src={product.images} alt={product.name} className=' h-1/2 self-center'/>
+        <p><span className=' font-bold text-lg'>{product.name}</span></p>
+  
+        <div className='md:flex items-center w-full justify-between'>
+          <ReactStars {...options}/> <span className=' font-extralight text-sm'>(256 reviews)</span>
+        </div>
+  
+        <span className=' font-bold text-lg mb-2'>${product.price}</span>
+  
+        <Link to={'/cart'} className=' bg-blue-500 text-white hover:bg-blue-600 py-4 w-full rounded-lg text-center font-bold'>Add To Cart</Link>
+      </Link>
+      </>
+    )
+  }
 }
 
 export default Product
