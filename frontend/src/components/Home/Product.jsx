b055b1f4908455fad14aac3b545b85b2ac89382b
Aayush <aayushman950@gmail.com>
2024-01-01 18:35:13 +0545

    added to cart message when adding to cart

--- frontend/src/components/Home/Product.jsx
+++ frontend/src/components/Home/Product.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import ReactStars from "react-rating-stars-component"
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchUserData } from '../../redux/userSlice';
+
 
 export const options = {
   edit: false,
@@ -22,36 +23,46 @@ const Product = ( {product} ) => {
     dispatch(fetchUserData());
   }, [dispatch]);
 
+  const [buttonText, setButtonText] = useState("Add to Cart")
+
   if (!user){
     return (
       <>
-      <Link to={`/product/:${product.id}`} className=' bg-gray-300 flex flex-col items-start justify-center p-2 md:w-[calc(14vmax)] border-2 hover:border-blue-500 w-[calc(40vmin)] min-h-[calc(20vmin)]'>
-        <img src={product.images} alt={product.name} className=' h-1/2 self-center'/>
-        <p><span className=' font-bold text-lg'>{product.name}</span></p>
-  
-        <div className='md:flex items-center w-full justify-between'>
-          <ReactStars {...options}/> <span className=' font-extralight text-sm'>(256 reviews)</span>
-        </div>
-  
-        <span className=' font-bold text-lg mb-2'>${product.price}</span>
-      </Link>
+      <div className=' bg-gray-300 flex flex-col items-center justify-between p-2 md:w-[calc(14vmax)] border-2 hover:border-blue-500 w-[calc(40vmin)] min-h-[calc(25vmax)]'>
+        <Link to={`/product/:${product.id}`} className='flex flex-col gap-'>
+          <img src={product.images} alt={product.name} className=' h-1/2 self-center'/>
+          <div className=' h-1/4'>
+            <p><span className=' font-bold text-lg'>{product.name}</span></p>
+      
+            <div className='md:flex items-center w-full justify-between'>
+              <ReactStars {...options}/> <span className=' font-extralight text-sm'>(256 reviews)</span>
+            </div>
+      
+            <span className=' font-bold text-lg'>${product.price}</span>
+          </div>
+        </Link>
+        <Link to={'/login'} className=' pb-4 bg-blue-500 text-white hover:bg-blue-600 py-4 w-full rounded-lg text-center font-bold'>{buttonText}</Link>
+      </div>
       </>
     )
   } else {
     return (
       <>
-      <Link to={`/product/:${product.id}`} className=' bg-gray-300 flex flex-col items-start justify-center p-2 md:w-[calc(14vmax)] border-2 hover:border-blue-500 w-[calc(40vmin)] min-h-[calc(20vmin)]'>
-        <img src={product.images} alt={product.name} className=' h-1/2 self-center'/>
-        <p><span className=' font-bold text-lg'>{product.name}</span></p>
-  
-        <div className='md:flex items-center w-full justify-between'>
-          <ReactStars {...options}/> <span className=' font-extralight text-sm'>(256 reviews)</span>
-        </div>
-  
-        <span className=' font-bold text-lg mb-2'>${product.price}</span>
-  
-        <Link to={'/cart'} className=' bg-blue-500 text-white hover:bg-blue-600 py-4 w-full rounded-lg text-center font-bold'>Add To Cart</Link>
-      </Link>
+      <div className=' bg-gray-300 flex flex-col items-center justify-between p-2 md:w-[calc(14vmax)] border-2 hover:border-blue-500 w-[calc(40vmin)] min-h-[calc(25vmax)]'>
+        <Link to={`/product/:${product.id}`} className='flex flex-col gap-'>
+          <img src={product.images} alt={product.name} className=' h-1/2 self-center'/>
+          <div className=' h-1/4'>
+            <p><span className=' font-bold text-lg'>{product.name}</span></p>
+      
+            <div className='md:flex items-center w-full justify-between'>
+              <ReactStars {...options}/> <span className=' font-extralight text-sm'>(256 reviews)</span>
+            </div>
+      
+            <span className=' font-bold text-lg'>${product.price}</span>
+          </div>
+        </Link>
+        <button onClick={() => setButtonText("Added to Cart")} className=' pb-4 bg-blue-500 text-white hover:bg-blue-600 py-4 w-full rounded-lg text-center font-bold'>{buttonText}</button>
+      </div>
       </>
     )
   }
