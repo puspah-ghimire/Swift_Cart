82c7397138548ff510b2ce94defb5bd5a89f0586
Aayush <aayushman950@gmail.com>
2024-01-01 18:02:38 +0545

    asked to login to buy

--- frontend/src/components/Product/ProductDetails.jsx
+++ frontend/src/components/Product/ProductDetails.jsx
@@ -4,10 +4,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchProductDetails } from '../../redux/productSlice';
 import ReactStars from 'react-rating-stars-component';
 import { options } from '../Home/Product';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { fetchUserData } from '../../redux/userSlice';
+
 
 const ProductDetails = () => {
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.userDetails);
+
+  useEffect(() => {
+    dispatch(fetchUserData());
+  }, [dispatch]);
+
   const { id } = useParams();
   const cleanedId = id.replace(':', '');
   const productDetails = useSelector((state) => state.products.productDetails);
@@ -17,38 +25,70 @@ const ProductDetails = () => {
   }, [dispatch, cleanedId]);
 
   if (!productDetails) {
-    return <p>Loading...</p>; // You can replace this with a loading spinner or component
+    return <p>Loading...</p>;
+  } else if (!user) {
+    return (
+      <>
+        <div className='flex flex-col md:flex-row md:p-12 md:py-20 p-6'>
+          <div className='md:w-1/2 mb-4'>
+            <Carousel className='border-2 h-[calc(50vmin)]'>
+              {productDetails && (
+                <img src={productDetails.images} alt={`Slide`} key={productDetails.id} />
+              )}
+            </Carousel>
+          </div>
+          <div className='flex flex-col md:mx-14'>
+            <div className='mb-4 flex flex-col'>
+              <p className='font-bold text-xl'>{productDetails.name}</p>
+              <p className='font-bold text-2xl'>${productDetails.price}</p>
+              <p>{productDetails.description}</p>
+              <div>
+                <ReactStars {...options} />
+              </div>
+              <p>Category: {productDetails.category}</p>
+              <p>Brand: {productDetails.brand}</p>
+              <p>Stock: {productDetails.amountInStock}</p>
+            </div>
+            <Link to="/login" className='bg-blue-800 hover:bg-blue-900 text-white w-fit px-4 font-bold py-2 rounded-lg'>
+              Login to Buy
+            </Link>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+
+    return (
+      <>
+        <div className='flex flex-col md:flex-row md:p-12 md:py-20 p-6'>
+          <div className='md:w-1/2 mb-4'>
+            <Carousel className='border-2 h-[calc(50vmin)]'>
+              {productDetails && (
+                <img src={productDetails.images} alt={`Slide`} key={productDetails.id} />
+              )}
+            </Carousel>
+          </div>
+          <div className='flex flex-col md:mx-14'>
+            <div className='mb-4 flex flex-col'>
+              <p className='font-bold text-xl'>{productDetails.name}</p>
+              <p className='font-bold text-2xl'>${productDetails.price}</p>
+              <p>{productDetails.description}</p>
+              <div>
+                <ReactStars {...options} />
+              </div>
+              <p>Category: {productDetails.category}</p>
+              <p>Brand: {productDetails.brand}</p>
+              <p>Stock: {productDetails.amountInStock}</p>
+            </div>
+            <Link to={'/cart'} className='bg-blue-800 hover:bg-blue-900 text-white w-fit px-4 font-bold py-2 rounded-lg'>
+              Add to Cart
+            </Link>
+          </div>
+        </div>
+      </>
+    );
   }
 
-  return (
-    <>
-      <div className='flex flex-col md:flex-row md:p-12 md:py-20 p-6'>
-        <div className='md:w-1/2 mb-4'>
-          <Carousel className='border-2 h-[calc(50vmin)]'>
-            {productDetails && (
-              <img src={productDetails.images} alt={`Slide`} key={productDetails.id} />
-            )}
-          </Carousel>
-        </div>
-        <div className='flex flex-col md:mx-14'>
-          <div className='mb-4 flex flex-col'>
-            <p className='font-bold text-xl'>{productDetails.name}</p>
-            <p className='font-bold text-2xl'>${productDetails.price}</p>
-            <p>{productDetails.description}</p>
-            <div>
-              <ReactStars {...options} />
-            </div>
-            <p>Category: {productDetails.category}</p>
-            <p>Brand: {productDetails.brand}</p>
-            <p>Stock: {productDetails.amountInStock}</p>
-          </div>
-          <button className='bg-blue-800 hover:bg-blue-900 text-white w-fit px-4 font-bold py-2 rounded-lg'>
-            Add to Cart
-          </button>
-        </div>
-      </div>
-    </>
-  );
 };
 
 export default ProductDetails;
